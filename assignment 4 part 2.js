b436b4fe28b638fb5569263e5f66194b9b01dd76
Cathrine Hertenstein <chertenstein@berklee.edu>
2019-02-12 11:06:55 -0500

    Assignment 4
    First attempt as Assignment 4!

---
+++ assignment 4 part 2.js
@@ -0,0 +1,26 @@
+let my_song = [
+  {note: 'A', starts_at: 0, lasts: 3},
+  {note: 'B', starts_at: 0, lasts: 3},
+  {note: 'C', starts_at: 1.5, lasts: 1.5},
+];
+console.log("n");
+play_piano(my_song, 3);
+
+function play_piano(song){
+  let start_time = 0;
+  let song_sorted = song_sorted(song);
+  for (let i = 0, i <= song; i++)
+  {
+    for (let item of song_sorted)
+  }
+  }
+if(item.starts_at = i){
+  console.log("play" + item.note);
+}
+if(i > 0 && item.lasts < song)
+console.log("hold for" + item.lasts + "seconds");
+
+if((item.starts_at + item.lasts) = i)
+{
+  console.log("release" + item.note);
+}
