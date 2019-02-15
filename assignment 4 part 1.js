89101b5c8258187681f9b840e009a1e99ef4d475
Cathrine Hertenstein <chertenstein@berklee.edu>
2019-02-15 11:28:18 -0500

    Assignment 4 Attempt 2
    Second attempt at Assignment 2!

--- assignment 4 part 1.js
+++ assignment 4 part 1.js
@@ -1,13 +1,13 @@
 let test_set = [1, 2, 3], new_set;
 
-function myset_add(data, new_value){
-  new_set = myset_add(test_set, 3);
-  if(arrays_equal(new_set.sort(myset_add), [1, 2, 3])) {
+function myset_add(){
+  new_set = myset_add.push(test_set, 3);
+  if(arrays_equal(new_set.sort(myset_add.push(new_set)), [1, 2, 3])) {
     console.log("Failed adding existing data");
   }
 }
 
-new_set = myset_add(test_set, 4);
+new_set = myset_add.push(test_set, 4);
 if(!arrays_equal(new_set.sort(test_set), [1, 2, 3, 4])){
   console.log("Failed adding new data");
 }
@@ -15,6 +15,5 @@ if(!arrays_equal(new_set.sort(test_set), [1, 2, 3, 4])){
 function myset_remove(data, remove_value){
 
 }
-//okay so I know that we are trying to add and remove data but I'm not sure how
-//to go about it. Also I'm getting an error that reads: maximum call stack size exceeded
-//and I don't know what that means...
+if(myset_add == "1" || "2" || "3" || "4")
+console.log("Duplicate number")
--- assignment 4 part 2.js
+++ assignment 4 part 2.js
@@ -10,17 +10,16 @@ function play_piano(song){
   let start_time = 0;
   let song_sorted = song_sorted(song);
   for (let i = 0, i <= song; i++)
-  {
-    for (let item of song_sorted)
-  }
+    for (let item of song_sorted);
   }
 if(item.starts_at = i){
   console.log("play" + item.note);
 }
-if(i > 0 && item.lasts < song)
+if(i > 0 && item.lasts < song.length)
 console.log("hold for" + item.lasts + "seconds");
 
 if((item.starts_at + item.lasts) = i)
-{
   console.log("release" + item.note);
-}
+
+  //Okay I think I have some of it but I get an error saying that "<="
+  //is an unexpeceted token?
