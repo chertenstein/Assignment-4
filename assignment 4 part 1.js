ac0f145d0ad44ac33c7df41be4eb72476afbd748
Cathrine Hertenstein <chertenstein@berklee.edu>
2019-03-15 21:05:36 -0400

    Assignment 4 try 4

--- assignment 4 part 1.js
+++ assignment 4 part 1.js
@@ -1,20 +1,28 @@
-let test_set = [1, 2, 3], new_set;
-myset_add(test_set, 4);
+let test_set = [1, 2, 3];
+console.log(myset_add(test_set, 4));
 
 function myset_add(myArray, newValue){
-  let new_set = myArray.push(test_set, 3);
-  if(myArray=(myArray.push(myArray.push(new_set)), [1, 2, 3])) {
-    console.log("Failed adding existing data");
+  for(let i = 0; i<= myArray.length; i++){
+    if(myArray[i] == newValue){
+      return myArray;
+    }
+  }
+  myArray.push(newValue);
+  return myArray;
+  }
+
+//create new myArray
+let new_set = [3, 4, 5, 6];
+function new_set_remove(myArray, newValue){
+  if(newValue = (test_set.push(myArray[i]), [1, 2, 3, 4])){
+    console.log(new_set);
+//loop through old myArray
+//if current value (myArray[i]) is the remove value, skip itfunction myset_remove(data, remove_value){
+  for(let i = 0; i <= new_set.length; i++);
+    if(new_set == myArray[i]){
+    return new_set.remove(myArray[i]);
+}else{
+  return new_set.push(myArray[i]);
   }
 }
-
-let new_set2 = test_set.push(test_set, 4);
-if(newValue=(test_set.push(test_set), [1, 2, 3, 4])){
-  console.log("Failed adding new data");
-}
-
-function myset_remove(data, remove_value){
-
-}
-if(newValue == "1" || "2" || "3" || "4")
-console.log("Duplicate number")
+//else, add the value to the new array
